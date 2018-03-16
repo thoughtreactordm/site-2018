7d23da2ba946881226be1fff60ad94ceadde99e4
Michael DeLally <thoughtreactor@gmail.com>
2018-03-16 16:42:12 -0400

    LOOK AT ME! Ready for production...

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,9 +3,23 @@
 @section('title', 'Engaging Indie Games')
 
 @section('content')
-    @foreach($posts as $post)
-        @include('blog.list')
-    @endforeach
+    @if(count($posts))
+        @foreach($posts as $post)
+            @include('blog.list')
+        @endforeach
+    @else
+        <section class="hero is-medium">
+            <div class="hero-body has-text-centered">
+                <img src="/img/meeseeks.png" alt="Oooo yeah!">
+                <h4 class="title is-1">
+                    He's tryin'!
+                </h4>
+                <h5 class="subtitle is-3">
+                    The site is live but I'm still working on the first post! Stay tuned.
+                </h5>
+            </div>
+        </section>
+    @endif
 
     {{ $posts->links('vendor.pagination.simple-default') }}
 @endsection
